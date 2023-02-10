554fb54e0e6fe0d67e4f42de2bea9ae7b1ae82c7
ABHISHEK KUMAR THAKUR <121752846+abhishekth0203@users.noreply.github.com>
2023-02-10 10:11:40 +0530

    1-part

---
+++ src/Component/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <section className='header-block'>
+            <header>play</header>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/Component/Top.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+export default class Top extends Component {
+    render() {
+        return (
+            <div>
+               <section className="top-block">
+                   <article>
+                       <span>भारत (हिन्दी)</span>
+                       <span className="x">X</span>
+                    </article>
+               </section>
+            </div>
+        );
+    }
+}
